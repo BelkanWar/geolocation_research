373f573a7d44e56fc27bc4e930dcd31de9658e2e
BelkanWar <challenger1501@gmail.com>
2022-12-25 22:00:54 +0800

    2022/12/25 progress

--- src/timeframe_traffic_density.py
+++ src/timeframe_traffic_density.py
@@ -1,12 +1,17 @@
 import os
 os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import datetime
-import csv
 import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+norm = 'log'
+
+for root, folder, files in os.walk("timeframe_density/"):
+    for file in files:
+        os.remove(os.path.join(root, file))
 
 data = pd.read_csv("result/output.csv", index_col=False, usecols=['start_time','lat_first','lon_first','moving status'])
 data['timestamp'] = [datetime.datetime.strptime(i,"%Y-%m-%d %H:%M:%S") for i in data['start_time'].tolist()]
@@ -20,14 +25,19 @@ lon_list.sort()
 lat_to_idx_dict = {lat_list[i]:i for i in range(len(lat_list))}
 lon_to_idx_dict = {lon_list[i]:i for i in range(len(lon_list))}
 
+contour = np.zeros((len(lat_list), len(lon_list)))
+
+for lat, lon, moving in zip(data['lat_first'], data['lon_first'], data['moving status']):
+    if moving == 1:
+        contour[lat_to_idx_dict[lat], lon_to_idx_dict[lon]] += 1
+
+img = plt.contour(contour, cmap='RdGy', norm=norm)
+levels = img.levels.copy()
+
 TIME_FRAME_INTERVAL = 3600
 START_TIME = min(data['timestamp']).replace(minute=0, second=0, microsecond=0)
 END_TIME = max(data['timestamp'])
 
-
-time_frame_to_idx_dict = {
-    START_TIME+datetime.timedelta(seconds=TIME_FRAME_INTERVAL*i):i 
-        for i in range(math.ceil((END_TIME-START_TIME).total_seconds()/TIME_FRAME_INTERVAL)+1)}
 
 for T_idx in range(math.ceil((END_TIME-START_TIME).total_seconds()/TIME_FRAME_INTERVAL)+1):
     subset = data.loc[data['timestamp'] >= START_TIME+datetime.timedelta(seconds=TIME_FRAME_INTERVAL*T_idx)]
@@ -42,18 +52,16 @@ for T_idx in range(math.ceil((END_TIME-START_TIME).total_seconds()/TIME_FRAME_IN
     
     plt.plot()
     try:
-        
-
-        if 'colormap' not in dir():
-            img = plt.contour(contour, norm='log')
-            colormap = plt.get_cmap().colors.copy()
+        if 'levels' not in dir():
+            img = plt.contour(contour, cmap='RdGy', norm=norm)
+            levels = img.levels.copy()
         else:
-            img = plt.contour(contour, colors=colormap, norm='log')
+            img = plt.contour(contour, cmap='RdGy', levels=levels)
 
         plt.savefig(f"timeframe_density/timeframe_density_{time_str}.png")
         
-    except Exception:
-        print(T_idx, np.sum(contour))
+    except Exception as error:
+        print(error)
     plt.close()
 
 
